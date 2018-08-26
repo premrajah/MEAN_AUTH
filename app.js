58c9be2a5d1003705fa12195284cb5444b5b2d0a
Prem Rajah <prajah@ctlondon.com>
2018-08-27 00:22:05 +0100

    finished connect to mongo db

--- app.js
+++ app.js
@@ -5,14 +5,47 @@ const cors = require("cors");
 const passport = require("passport");
 const mongoose = require("mongoose");
 
+// local
+const config = require('./config/database');
+
+// connect to database
+mongoose.connect(config.database);
+
+// on db connection
+mongoose.connection.on('connected', () => {
+    console.log('connected to db ', config.database);
+});
+
+// on db connection error
+mongoose.connection.on('error', (err) => {
+    console.log('db error:  ', err);
+});
+
 const app = express();
+
+const users = require('./routes/users');
+
+// port number for page
 const port = 3000;
 
-//temp
+// uses cores module for cross-domain
+app.use(cors());
+
+// set static folder files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// body-parser middleware
+app.use(bodyParser.json());
+
+app.use("/users", users);
+
+//temp - index route
 app.get("/", (req, res) => {
     res.send("Invalid endpoint.");
 });
 
+
+// start server
 app.listen(port, () => {
     console.log("Server started on port: ", port);
 });
