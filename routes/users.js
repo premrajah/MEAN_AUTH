ca0659c6e398702ecbd06e26a043da1c537bec5b
Prem Rajah <prajah@ctlondon.com>
2018-08-28 09:34:00 +0100

    Added users

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 
 // reqister route
 router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.passport
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({
+                scccess: false,
+                msg: "Failed to register user"
+            });
+        } else {
+            res.json({
+                success: true,
+                msg: "Added user succussfully"
+            });
+        }
+    });
 });
 
 // Authenticate
